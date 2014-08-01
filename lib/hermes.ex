b1670fa21c046016b7ec4038b364012c72650f8f
Johnny Winn <johnny+nurugger07@traitify.com>
2014-08-01 00:13:32 -0400

    Send recipient & sender info through socket

--- lib/hermes.ex
+++ lib/hermes.ex
@@ -2,10 +2,7 @@ defmodule Hermes.Client do
 
   # %{domain: domain, port: port, user: user, pword: password, from: email_address, to: email_address, body: body}
   def send(options) do
-    options |> connect |> handshake |> authorize
-      # address |>
-      # send_mail |>
-      # quit
+    options |> connect |> initiate |> authorize |> address |> send_mail |> quit
   end
 
   def connect(options) do
@@ -13,15 +10,15 @@ defmodule Hermes.Client do
     Map.merge options, %{socket: socket}
   end
 
-  def handshake({:error, message}), do: { :error, message }
-  def handshake(options) do
+  def initiate({:error, message}), do: { :error, message }
+  def initiate(options) do
     :gen_tcp.send(options.socket, "EHLO")
     receive do
       { :tcp, socket, << "220", _ :: binary>> } ->
         options
     after
       500 ->
-        { :error, "Unable to establish a connection" }
+        { :error, options.socket, "Unable to establish a connection" }
     end
   end
 
@@ -33,13 +30,13 @@ defmodule Hermes.Client do
     receive do
       { :tcp, _, << "334", _ :: binary>> } ->
         submit_credentials(options, [ user, password ])
-      { :tcp, _, << "4", _ :: binary >> } ->
-        { :error, "Unable to autheticate. Trya again later" }
+      { :tcp, socket, << "4", _ :: binary >> } ->
+        { :error, socket, "Unable to autheticate. Trya again later" }
       { :tcp, _, << "500", _ :: binary >>} ->
         authorize options
     after
       12000 ->
-        { :error, "Connection timeout for authorization" }
+        { :error, options.socket, "Connection timeout for authorization" }
     end
   end
 
@@ -52,20 +49,64 @@ defmodule Hermes.Client do
         options
     after
       12000 ->
-        { :error, "Connection timeout for authorization" }
+        { :error, options.socket, "Connection timeout for authorization" }
     end
   end
 
-  def address({:error, message}), do: { :error, message }
+  def address({:error, socket, message}), do: { :error, socket, message }
   def address(options) do
+    options |> sender(options.from) |> recipients(options.to)
+  end
+
+  def sender(options, << "<", _ :: binary >> = from) do
+    :gen_tcp.send(options.socket, "MAIL FROM: #{from}\r\n")
+    receive do
+      { :tcp, socket, << "250", _ :: binary >> } ->
+        options
+    after
+      12000 ->
+        { :error, options.socket, "Connection timeout setting senders" }
+    end
+  end
+  def sender(options, from), do: sender(options, "<#{from}>")
+
+  def recipients(options, recipient) when is_binary recipient do
+    recipients(options, [recipient])
+  end
+  def recipients(options, []), do: options
+  def recipients(options, [ << "<", _ :: binary >> = recipient | t ]) do
+    :gen_tcp.send(options.socket, "RCPT TO: #{recipient}\r\n")
+    receive do
+      { :tcp, socket, << "250", _ :: binary >> } ->
+        recipients(options, t)
+      anything ->
+        IO.puts inspect anything
+    after
+      12000 ->
+        { :error, options.socket, "Connection timeout setting recipients" }
+    end
+  end
+  def recipients(options, [ recipient | t]) do
+    recipients(options, [ "<#{recipient}>" | t ])
+  end
+
+  def send_mail({:error, socket, message}), do: { :error, socket, message }
+  def send_mail(options) do
 
   end
 
-  def send_mail() do
-
+  def quit({:error, socket, message}) do
+    quit socket
+    { :error, message }
   end
 
-  def quit do
+  def quit({:ok, socket, message}) do
+    quit socket
+    { :ok, message }
+  end
 
+  def quit(options) do
+    :gen_tcp.send options.socket, "QUIT\r\n"
+    :gen_tcp.close options.socket
   end
 end
